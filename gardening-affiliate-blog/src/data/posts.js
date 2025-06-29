f208af397e0222efa320bc79a9e136c29aaeb524
SainDev <46877765+MonDroids@users.noreply.github.com>
2025-06-29 23:13:02 +0800

    test2

--- gardening-affiliate-blog/src/data/posts.js
+++ gardening-affiliate-blog/src/data/posts.js
@@ -1,1 +1,10 @@
-export const posts = [];
+export const posts = [
+    {
+      slug: "how-to-grow-tomatoes",
+      title: "How to Grow Tomatoes Easily",
+      excerpt: "Learn how to grow juicy tomatoes in your backyard...",
+      date: "2025-06-29",
+      image: "/images/tomatoes.jpg",
+      tags: ["vegetables", "gardening tips"]
+    }
+  ];
